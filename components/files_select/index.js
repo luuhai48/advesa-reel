33da71452f83552532ba9e27285acc0b6a2669d6
luuhai48 <luuhai.hn48@gmail.com>
2023-06-18 16:51:36 +0700

    Input if of google drive folder, then fetch files

---
+++ components/files_select/index.js
@@ -0,0 +1,25 @@
+if (data.length) {
+  var filesList = document.getElementById("files");
+  var newFiles = [];
+
+  data.forEach((file) => {
+    var elem = document.createElement("a");
+    elem.setAttribute("href", "#");
+    elem.dataset.fileId = file.id;
+    elem.classList.add("file");
+    elem.style.backgroundImage = `url(${file.thumbnailLink})`;
+
+    var icon = document.createElement("img");
+    icon.classList.add("icon");
+    icon.src = file.iconLink;
+    elem.appendChild(icon);
+
+    var title = document.createElement("span");
+    title.textContent = file.title;
+    elem.appendChild(title);
+
+    newFiles.push(elem);
+  });
+
+  filesList.append(...newFiles);
+}
--- js/main.js
+++ js/main.js
@@ -1,25 +1,222 @@
+var main;
+var scripts;
+var toasts;
+var secretCount = 0;
+var secretTimer;
+var data = [];
+var eventListeners = [];
+var historyStack = [];
+var selectableElements = [];
+var gid = localStorage.getItem("gid");
+
 function loading() {
-  document.getElementById("main").classList.add("loading");
-}
-function loadDone() {
-  document.getElementById("main").classList.remove("loading");
+  main.classList.add("loading");
 }
 
-window.onload = function () {
+function loadDone() {
+  main.classList.remove("loading");
+}
+
+function getSelectableElements() {
+  selectableElements = Array.from(
+    document.querySelectorAll("input, button, select, textarea, a[href]")
+  );
+}
+
+function selectNext() {
+  if (!selectableElements.length) return;
+
+  if (!document.activeElement || document.activeElement.tagName === "BODY") {
+    document.activeElement?.blur();
+    selectableElements[0].focus();
+    return;
+  }
+
+  let idx = selectableElements.indexOf(document.activeElement);
+  if (idx < 0) {
+    document.activeElement?.blur();
+    selectableElements[0].focus();
+    return;
+  }
+  if (idx === selectableElements.length - 1) return;
+  selectableElements[idx + 1].focus();
+}
+
+function selectPrev() {
+  if (!selectableElements.length) return;
+
+  if (!document.activeElement || document.activeElement.tagName === "BODY") {
+    document.activeElement?.blur();
+    selectableElements[selectableElements.length - 1].focus();
+    return;
+  }
+
+  let idx = selectableElements.indexOf(document.activeElement);
+  if (idx < 0) {
+    document.activeElement?.blur();
+    selectableElements[selectableElements.length - 1].focus();
+    return;
+  }
+  if (idx === 0) return;
+  selectableElements[idx - 1].focus();
+}
+
+async function loadComponent(name) {
+  loading();
+
+  if (eventListeners.length) {
+    eventListeners.forEach(({ event, handler }) => {
+      document.removeEventListener(event, handler);
+    });
+    eventListeners = [];
+  }
+
+  await fetch(`components/${name}/index.html`)
+    .then((res) => res.text())
+    .then((html) => {
+      main.innerHTML = html;
+    });
+  await fetch(`components/${name}/index.js`)
+    .then((res) => res.text())
+    .then((js) => {
+      var elem = document.createElement("script");
+      elem.textContent = js;
+      scripts.innerHTML = "";
+      scripts.appendChild(elem);
+    });
+
+  loadDone();
+  historyStack.push(name);
+
+  getSelectableElements();
+  if (
+    selectableElements.length &&
+    (!document.activeElement || document.activeElement.tagName === "BODY")
+  ) {
+    document.activeElement?.blur();
+    selectableElements[0].focus();
+  }
+}
+
+function notify(status, title, message) {
+  var toast = document.createElement("div");
+  toast.classList.add("toast", "active", status);
+  toast.innerHTML = `<div class="toast-content">
+  <i class="gg-check-o check"></i>
+  <div class="message">
+    <span class="text text-1">${title}</span>
+    <span class="text text-2">${message}</span>
+  </div>
+</div>
+<i class="gg-close close"></i>
+<div class="progress active"></div>`;
+  toasts.appendChild(toast);
+
+  setTimeout(() => {
+    toast.classList.remove("active");
+    setTimeout(() => {
+      toasts.removeChild(toast);
+    }, 500);
+  }, 5000);
+}
+
+async function fetchFolderInfo(gid) {
+  loading();
+  try {
+    const res = await fetch(`https://advesa-reel-api.deno.dev/${gid}`);
+    const json = await res.json();
+    loadDone();
+
+    if (res.status !== 200) {
+      notify("error", "Error", json?.error || "Failed to get folder info");
+      localStorage.removeItem("gid");
+
+      if (historyStack[historyStack.length - 1] !== "id_input") {
+        await loadComponent("id_input");
+      }
+      return;
+    }
+
+    data = json;
+    localStorage.setItem("gid", gid);
+
+    await loadComponent("files_select");
+  } catch (e) {
+    loadDone();
+    notify("error", "Error", e?.message || "Something went wrong");
+    if (historyStack[historyStack.length - 1] !== "id_input") {
+      await loadComponent("id_input");
+    }
+  }
+}
+
+function goBack() {
+  if (historyStack.length <= 1) {
+    tizen.application.getCurrentApplication().exit();
+    return;
+  }
+  historyStack.pop();
+
+  if (historyStack.length) {
+    var item = historyStack[historyStack.length - 1];
+    loadComponent(item);
+    if (item === "id_input") {
+      localStorage.removeItem("gid");
+    }
+  }
+}
+
+function secretInputHandler(e) {
+  // Press OK - 10 times
+  if (e.keyCode === 13) {
+    clearTimeout(secretTimer);
+
+    secretCount += 1;
+
+    if (secretCount === 10) {
+      var id_input = document.getElementById("id-input");
+      if (id_input) {
+        id_input.value = "1PXQWoOPXiV140oe-tO0svpjYSGK1Akqk";
+      }
+      return;
+    }
+
+    secretTimer = setTimeout(() => {
+      secretCount = 0;
+    }, 5000);
+  }
+}
+
+window.onload = async function () {
+  main = document.getElementById("main");
+  scripts = document.getElementById("scripts");
+  toasts = document.getElementById("toasts");
+
   document.addEventListener("keydown", function (e) {
     switch (e.keyCode) {
-      case 37: //LEFT arrow
+      case 37: {
+        //LEFT arrow
+        if (historyStack[historyStack.length - 1] === "id_input") break;
+        selectPrev();
         break;
+      }
       case 38: //UP arrow
+        selectPrev();
         break;
-      case 39: //RIGHT arrow
+      case 39: {
+        //RIGHT arrow
+        if (historyStack[historyStack.length - 1] === "id_input") break;
+        selectNext();
         break;
+      }
       case 40: //DOWN arrow
+        selectNext();
         break;
       case 13: //OK button
+        console.log("enter");
         break;
       case 10009: //RETURN button
-        tizen.application.getCurrentApplication().exit();
+        goBack();
         break;
       default:
         console.log("Key code : " + e.keyCode);
@@ -27,15 +224,8 @@ window.onload = function () {
     }
   });
 
-  var main = document.getElementById("main");
-
-  var gid = localStorage.getItem("gid");
-  if (!gid) {
-    fetch("components/id_input.html").then(async (res) => {
-      var html = await res.text();
-      main.innerHTML = html;
-      loadDone();
-    });
-    return;
+  await loadComponent("id_input");
+  if (gid) {
+    await fetchFolderInfo(gid);
   }
 };
